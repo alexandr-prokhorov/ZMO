503841a182cad7a4ddbe9f1b8fdf3c0eb7db820c
proho <prohorovalex0013@gmail.com>
2025-06-16 21:08:10 +0400

    зарегистрировал модели в админке

--- houses/admin.py
+++ houses/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from houses.models import House, HousePlan
+
+@admin.register(House)
+class HouseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'size', 'floor', 'bedroom', 'bathroom')
+    ordering = ('name',)
+
+
+@admin.register(HousePlan)
+class HousePlanAdmin(admin.ModelAdmin):
+    list_display = ('plan', 'order')
+    ordering = ('pk',)
