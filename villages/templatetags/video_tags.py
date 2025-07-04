2d18520ace467a70e576e262592f242c89141a25
proho <prohorovalex0013@gmail.com>
2025-07-05 00:49:39 +0400

    Добавлен паттерн для загрузки видео из ютуб и рутуб для получения из url id видео

---
+++ villages/templatetags/video_tags.py
@@ -0,0 +1,22 @@
+from django import template
+import re
+from urllib.parse import urlparse, parse_qs
+
+register = template.Library()
+
+
+@register.filter
+def get_video_embed_url(url):
+    # Для YouTube
+    youtube_match = re.match(r'(?:https?:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?([a-zA-Z0-9_-]+)', url)
+    if youtube_match:
+        video_id = youtube_match.group(1)
+        return f"https://www.youtube.com/embed/{video_id}"
+
+    # Для Rutube
+    rutube_match = re.match(r'(?:https?:\/\/)?(?:www\.)?rutube\.ru\/video\/([a-zA-Z0-9_-]+)', url)
+    if rutube_match:
+        video_id = rutube_match.group(1)
+        return f"https://rutube.ru/play/embed/{video_id}"
+
+    return url
